6349443023e93ff1b2bf99587e80136b59c6ae8b
salahbm <salahbm.001@gmail.com>
2023-12-14 22:47:33 +0900

    models

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import connectDB from './mongoDb/connect.js';
+import daleRoutes from './routes/daleRoutes.js';
+
 
 
 dotenv.config();
@@ -12,6 +14,10 @@ app.use(cors)
 app.use(express.json({
     limit: '50mb'
 }))
+
+
+// app.use('api/v1/post', postRoutes)
+app.use('api/v1/dalle', daleRoutes)
 
 app.get('/', async (req, res) => {
     res.send('Hello from server')
---
+++ server/routes/daleRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import {Configuration , OpenAI} from 'openai'
+
+
+
+dotenv.config()
+
+const router = express.Router()
