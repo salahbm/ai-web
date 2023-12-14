40191d2da47309988670408cd455caaf2de1b074
salahbm <salahbm.001@gmail.com>
2023-12-14 11:48:22 +0900

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+import { CreatePost, Home } from "./pages";
 
 const App = () => {
   return (
-    <div className=' font-bold text-lg'>App</div>
-  )
-}
+    <BrowserRouter>
+      <header className="w-full  flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="./">
+          <img src={logo} alt="Logo" className="w-28 object-contain" />
+        </Link>
+        <Link
+          to="./create-post"
+          className="font-inter font-medium bg-[#6469ff] text-white p-4 py-2 rounded-r-md"
+        >
+          {" "}
+          Create Post
+        </Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9f8fe] min-h-[cal(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
