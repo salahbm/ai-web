08aecc27dd3cff3f8b5a2685fad5f66eab9ee62c
salahbm <salahbm.001@gmail.com>
2023-12-20 00:51:19 +0900

    demo page

--- client/src/components/Demo.jsx
+++ client/src/components/Demo.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "./Hero";
-import { linkIcon } from "../assets";
+import { copy, linkIcon, loader } from "../assets";
 import { useLazyGetSummaryQuery } from "../redux/slicer/article";
 
 const Demo = () => {
@@ -8,10 +8,27 @@ const Demo = () => {
     url: "",
     summary: "",
   });
-  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery;
+  const [allArticles, setAllArticles] = useState([]);
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromStorage = JSON.parse(localStorage.getItem("articles"));
+    if (articlesFromStorage) {
+      setAllArticles(articlesFromStorage);
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
     const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data?.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
   };
   return (
     <section className="z-10 min-h-full">
@@ -42,6 +59,57 @@ const Demo = () => {
             </button>
           </form>
         </div>
+      </div>
+      <div className="flex flex-col gap-1 max-h-60 overflow-y-auto ">
+        {allArticles.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => setArticle(item)}
+            className="link_card"
+          >
+            <div className="copy_btn">
+              <img
+                src={copy}
+                alt="copy"
+                className="w-[40%] h-{40%] object-contain "
+              />
+            </div>
+            <p className="flex-1 font-satoshi text-purple-700  truncate text-sm font-medium">
+              {item.url}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img
+            src={loader}
+            alt="Loading"
+            className="w-20 h-20 object-contain"
+          />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, I guess there is the problem with Billing 💸 or API
+            fetching... 😩
+            <br />
+            <span className="font-satoshi font-normal text-red-300">
+              {error?.data.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
